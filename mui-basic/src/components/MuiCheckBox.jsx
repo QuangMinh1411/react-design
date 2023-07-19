5ffb2d9db61727bcf1aca6b37f87b992f0544c20
QuangMinh1411@gmail.com <tomminhquan1411@gmail.com>
2023-07-19 17:30:42 +0700

    Checkbox

--- mui-basic/src/components/MuiCheckBox.jsx
+++ mui-basic/src/components/MuiCheckBox.jsx
@@ -1,9 +1,11 @@
 import {useState} from 'react'
 import { Box,FormControlLabel,Checkbox } from '@mui/material'
+import BookmarkIcon from '@mui/icons-material/Bookmark'
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'
 const MuiCheckBox = () => {
     const [acceptTnc,setAcceptTnc] = useState(false)
     const handleChange = (e)=>{
-        setAcceptTnc(e.target.value)
+        setAcceptTnc(e.target.checked)
     }
     console.log(acceptTnc)
   return (
@@ -12,6 +14,14 @@ const MuiCheckBox = () => {
             <FormControlLabel label='I accept terms and conditions' 
             control={<Checkbox checked={acceptTnc} onChange={handleChange} />}/>
 
+        </Box>
+        <Box>
+          <Checkbox 
+            icon={<BookmarkBorderIcon />} 
+            checkedIcon={<BookmarkIcon />}
+            checked={acceptTnc}  
+            onChange ={handleChange}
+          />
         </Box>
     </Box>
   )
